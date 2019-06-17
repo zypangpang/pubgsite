34c31b56b4fd2d0ad80149c38cc2c0a14f21eced
潘海楠 <panhainan@panhainandeMacBook-Pro.local>
2019-06-17 18:43:33 +0800

    fix bug

--- main/views.py
+++ main/views.py
@@ -47,7 +47,10 @@ def room(request):
     r = request.GET['r']
     user_id = request.user.id
     models.Users.objects.filter(user_id=user_id).update(room_id=r)
-    return render(request, 'main/room.html')
+    userDtb = models.User.objects.raw("select a.id, a.name as room_name, group_concat(c.username) as user_name from main_room a left join main_users b on a.id = b.room_id left join auth_user c on c.id = b.user_id group by a.id")
+    context = {}
+    context['rooms'] = userDtb
+    return render(request, 'main/room.html', context)
 
 
 @login_required

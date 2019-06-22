fd862c0e1a6e1e47ee1462356b3ca944a6842f3b
潘海楠 <panhainan@panhainandeMacBook-Pro.local>
2019-06-22 20:27:18 +0800

    merge all code

--- main/urls.py
+++ main/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('game/get_state/', views.get_cur_state, name='get_state'),
     path('game/init/', views.get_cur_state, name='init'),
     path('game/auth/', views.authenticate, name='auth'),
-    path('game/commander/', views.chooseCommander, name='commander'),
+    path('game/commander/', views.choose_cmd, name='commander'),
     path('game/house/', views.open_house, name='open_house'),
     path('game/gameover/', views.game_over, name='game_over'),
     path('game/<slug:name>/', views.game, name='game'),
@@ -28,18 +28,6 @@ urlpatterns = [
 
     # 跳伞接口
     path('parachute', views.parachute, name='parachute'),
-
-    # 人物移动接口
-    path('move', views.move, name='move'),
-
-    # 认证接口
-    path('certificate', views.certificate, name='certificate'),
-
-    # 选择指挥官接口
-    path('game/commander', views.choose_cmd, name='commander'),
-
-    # 开箱子接口
-    path('open-box', views.open_box, name='open-box'),
 
     # 数据初始化接口
     path('init', views.init, name='init'),
--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ import json,random
 from django.shortcuts import render
 
 import csv, bisect, json, logging, random
-import rsa as yynrsa
+import rsa
 from django.conf import settings
 from django.contrib.auth.models import User, Group
 # from django.core.files import File
@@ -14,7 +14,12 @@ from django.contrib.auth.decorators import permission_required
 
 from django.http import HttpResponse, Http404
 
-from main import models, prime
+import json,random
+from django.shortcuts import render
+from django.contrib.auth.models import User
+
+import models, prime, lagrange
+import initialization
 
 logger = logging.getLogger(__name__)
 PAGE_SIZE = 10
@@ -149,14 +154,186 @@ def choose_cmd(request):
 
 
 @login_required
-def open_box(request):
-    return 1
-
-
-@login_required
 def init(request):
-    return 1
+    '''
+    :param player_id: the player_id is expected to be in [0, 4]
+    :return:
+    '''
+    player_id=request.user.id-1
+    position_list = []
+    for i in range(1, 6):
+        position_x = initialization.get_player_attribute(i, 'position_x')
+        position_y = initialization.get_player_attribute(i, 'position_y')
+        position_list.append((position_x, position_y))
 
+    user_name_list = ['pangzaiyu', 'yangyinuo', 'xujunzhou', 'gaoming', 'panhainan']
+    res_dict = {}
+    other_player = {}
+    for i in range(0, 5):
+        if(i != player_id):
+            info = {}
+            info['position'] = [position_list[i][0], position_list[i][1]]
+            info['known'] = 0
+            other_player[user_name_list[i]] = info
+    res_dict['player'] = user_name_list[player_id]
+    res_dict['position'] = [position_list[player_id][0], position_list[player_id][1]]
+    res_dict['other_players'] = other_player
+    return res_dict
+
+def get_cur_state(request):
+    username = request.user.get_username()
+    user_id = request.user.id
+    user = models.Users.objects.get(user_id=user_id)
+
+    user.position_x = request.POST['position_x']
+    user.position_y = request.POST['position_y']
+    user.save()
+
+    user_list = models.Users.objects.all()
+    global_status = models.SystemParam.objects.get(key='global_status')
+    other_players = {}
+    known_count = 0
+    for u in user_list:
+        same_group = 0
+        if u.user_id == user_id:
+            continue
+        elif u.group_id == user.group_id:
+            same_group = 1
+            known_count += 1
+        other_players[u.user_id] = {'position': [u.position_x, u.position_y],
+                                    'known': same_group}
+
+    if (known_count >= 4) and (global_status.intValue <= 1):
+        global_status.intValue = 2
+        global_status.save()
+
+    return_dict={
+        'player': username,
+        'position': [user.position_x, user.position_y],
+        'can_choose_commander': 1 if global_status.intValue == 2 else 0,
+        'otherPlayers': other_players
+    }
+
+    if(user.certificating_with > 0):
+        c_username = User.objects.get(id=user.certificating_with).get_username()
+        return_dict['event'] = {'name': 'auth',
+                                'username': c_username,
+                                'info': 'you are certificating with ' + c_username + '.'}
+    if(user.opening_box > 0):
+        return_dict['event'] = {'name': 'open_house',
+                                'house_id': user.opening_box,
+                                'info': 'you are opening box ' + str(user.opening_box) + '.'}
+
+    global_status = models.SystemParam.objects.get(key='global_status').intValue
+    #TODO: fetch candidates list
+    if global_status == 3:
+        if user.vote_to <= 0:
+            candidates = ['junzhou','hainan']
+            return_dict['event'] = {'name': 'vote_commander',}
+                                   # 'candidates': candidates,
+                                    #'info': 'vote for commander. options: ' + ', '.join(candidates) + '.'}
+    elif global_status == 5:
+        return_dict['event'] = {'name': 'game_over',
+                                'end': 1,
+                                'info': 'you enter the house and find lot\'s of shinning weapons.'}
+    elif global_status == 6:
+        return_dict['event'] = {'name': 'game_over',
+                                'end': 0,
+                                'info': 'you enter the house. suddenly the house explode. you die.'}
+
+    return HttpResponse(json.dumps(return_dict))
+
+
+def authenticate(request):
+    '''
+        certificate_result: 1 success, 0 fail
+    '''
+    from_id = request.user.id
+    user = models.Users.objects.get(user_id=request.user.id)
+    to_id = request.POST['to_id']
+
+    user.certificating_with = to_id
+    user.save()
+
+    result = rsa.two_way_certificate(from_id, to_id)
+    info = ""
+    if result == 1:
+        rsa.merge_user_group(from_id, to_id)
+        info = "certificate with user " + str(to_id) + " succeed."
+    else:
+        info = "certificate with user " + str(to_id) + " fail."
+    return_dict = {'success': result,
+                   'info': info}
+
+    user.certificating_with = -1
+    user.save()
+    return HttpResponse(json.dumps(return_dict))
+
+def open_house(request):
+    user_id = request.user.id
+    user = models.Users.objects.get(user_id=request.user.id)
+    position = (user.position_x, user.position_y)
+    box_id = -1
+
+    '''
+    house 1: (3,21-24)(4-6,22-24)
+    house 2: (20,3-6)(21-23,4-6)
+    '''
+    house_range = []
+    house_range.append([(2,y) for y in range(20,26)] + [(x,25) for x in range(3,8)] +
+                       [(7,y) for y in range(21,25)] + [(x,21) for x in range(4,7)] +
+                       [(3,20), (4,20)])
+    house_range.append([(19,y) for y in range(2,8)] + [(x,7) for x in range(20,25)] +
+                       [(24,y) for y in range(3,7)] + [(x,3) for x in range(21,24)] +
+                       [(20,2), (21,2)])
+    for i in range(len(house_range)):
+        if position in house_range[i]:
+            box_id = i
+            break
+
+    if box_id < 0:
+        return_dict = {'success': 0,
+                       'info': 'you are not close to any house.'}
+        return HttpResponse(json.dumps(return_dict))
+
+    user.opening_box = box_id
+    user.save()
+
+    user_list = models.Users.objects.all()
+    key_list = []
+    box = models.Box.objects.get(id=box_id)
+    for u in user_list:
+        if u.group_id == user.group_id:
+            if (u.position_x, u.position_y) in house_range[box_id]:
+                key_list.append((u.box_key_x, u.box_key_y))
+
+    solve_result = lagrange.solve_lagrange(key_list, box.least_num)
+    result = 0
+    info = "open fail"
+    if solve_result == -1:
+        result = 0
+        info = "need more people to open the box"
+    elif solve_result == -2:
+        result = 0
+        info = "the key is conflict"
+    elif solve_result == box.password:
+        result = 1
+        info = "open succeed"
+
+    return_dict = {'success': result,
+                   'info': info}
+    user.opening_box = -1
+    user.save()
+    return HttpResponse(json.dumps(return_dict))
+
+def game_over(request):
+    ending = request.POST['ending']
+    global_status = models.SystemParam.objects.get(key='global_status')
+    if ending == 1:
+        global_status.intValue = 5
+    elif ending == 0:
+        global_status.intValue = 6
+    global_status.save()
 
 # 自定义方法
 def get_return_dict_for_navbar(request):

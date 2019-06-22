f05a623608dbdf108f200d30fe38ae2de5652b85
xuxu9110 <xujunzhou14@163.com>
2019-06-22 17:25:31 +0800

    add views_xjz.py

--- main/rsa.py
+++ main/rsa.py
@@ -4,14 +4,16 @@ import prime
 import time
 import re
 
-#from . import models
+import models
 
-def encryption(message, prk):
-    '''Encrypt the int message into an int number using prk'''
-    return pow(message, prk[1], prk[0])
+#public_key = e, private_key = d, rsa_n = n
 
-def segment_encryption(message, seg_len, prk):
-    '''Encrypt the string message into an int list using prk'''
+def encryption(message, n, d):
+    '''Encrypt the int message into an int number using prk[n,d]'''
+    return pow(message, d, n)
+
+def segment_encryption(message, seg_len, n, d):
+    '''Encrypt the string message into an int list using prk[n,d]'''
     result = []
     msg_len = len(message)
     i = 0
@@ -21,19 +23,19 @@ def segment_encryption(message, seg_len, prk):
         msg_num = 0
         for j in range(lo_bound, up_bound):
             msg_num = (msg_num << 8) + ord(message[j])
-        result.append(pow(msg_num, prk[1], prk[0]))
+        result.append(pow(msg_num, d, n))
         i += 1
     return result
 
-def decryption(secret, puk):
-    '''Decrypt the int message into an int number using puk'''
-    return pow(secret, puk[1], puk[0])
+def decryption(secret, n, e):
+    '''Decrypt the int message into an int number using puk[n, e]'''
+    return pow(secret, e, n)
 
-def segment_decryption(secret, puk):
-    '''Decrypt the int list into a string message using puk'''
+def segment_decryption(secret, n, e):
+    '''Decrypt the int list into a string message using puk[n, e]'''
     result = ""
     for secret_seg in secret:
-        msg_seg = pow(secret_seg, puk[1], puk[0])
+        msg_seg = pow(secret_seg, e, n)
         temp = ""
         while msg_seg > 0:
             temp += chr(msg_seg & 0xFF)
@@ -56,22 +58,24 @@ def get_RSAKeyList(n, count):
         s = (p - 1) * (q - 1)
         e = e_arr[i]
         d = prime.mod_inverse(e, s)
+        '''
         print("p = ", p, ",q = ", q)
         print("n = ", n)
         print("e = ", e, ",d = ", d)
-        puk = [n, e]
-        prk = [n, d]
-        RSAKey['puk'] = puk
-        RSAKey['prk'] = prk
+        '''
+        RSAKey['n'] = n
+        RSAKey['e'] = e
+        RSAKey['d'] = d
         RSAKeyList.append(RSAKey)
     return RSAKeyList
 
-def check(from_id, to_id):
-    '''input the from_id and to_id, return the check is True or False'''
+def two_way_certificate(from_id, to_id):
+    '''input the from_id and to_id, if succeed then return 1, else return 0'''
     user_id = [from_id, to_id]
     user = []
-    puk = []
-    prk = []
+    n = []
+    e = []
+    d = []
     room_id = []
     massage = []
     secret = []
@@ -80,28 +84,44 @@ def check(from_id, to_id):
 
     for i in range(0,2):
         try:
-            user.append(models.Users.objects.get(id=user_id[i]))
+            user.append(models.Users.objects.get(user_id=user_id[i]))
         except:
-            return False
+            return 0
 
     for i in range(0,2):
-        puk.append(user[i].public_key)
-        prk.append(user[i].private_key)
+        e.append(user[i].public_key)
+        d.append(user[i].private_key)
+        n.append(user[i].rsa_n)
         room_id.append(user[i].room.id)
         massage.append("hello, this is private no.{} from room {}, "
             "the time is {}".format(user_id[i], room_id[i], time.time()))
-        secret.append(segment_encryption(massage[i], 40, prk[i]))
-        decrypted_massage.append(segment_decryption(massage[i], puk[i]))
+        secret.append(segment_encryption(massage[i], 40, n[i], d[i]))
+        decrypted_massage.append(segment_decryption(massage[i], n[i], e[i]))
         try:
             a = re.search("room [0-9]+", decrypted_massage[i])
             decrypted_room_id.append(a.group(0))
         except:
-            return False
+            return 0
 
     if decrypted_room_id[0] == decrypted_room_id[1]:
-        return True
+        return 1
     else:
-        return False
+        return 0
+
+def merge_user_group(id1, id2):
+    #change all user with group_id=max(id1.group_id, id2.group_id) to min(,)
+    group1 = models.Users.objects.get(user_id=id1).group_id
+    group2 = models.Users.objects.get(user_id=id2).group_id
+    if group1 == group2:
+        return 1
+    else:
+        from_group = max(group1, group2)
+        to_group = min(group1, group2)
+    user_list = models.Users.objects.all()
+    for u in user_list:
+        if u.group_id == from_group:
+            u.group_id = to_group
+            u.save()
 
 '''
 if __name__ == '__main__':
@@ -114,8 +134,8 @@ if __name__ == '__main__':
     print("Enter the message: ")
     message = input()
     print("Message: ", message)
-    secret = segment_encryption(message, 80, RSAKey['prk']) #less than n/8
+    secret = segment_encryption(message, 80, RSAKey['n'], RSAKey['d']) #less than n/8
     print("Encrypted message: ", secret)
-    message = segment_decryption(secret, RSAKey['puk'])
+    message = segment_decryption(secret, RSAKey['n'], RSAKey['e'])
     print("Decrypted message: ", message)
 '''
